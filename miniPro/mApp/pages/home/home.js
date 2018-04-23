c804a02a06603f508d37624334c5bad9f602a1ce
mooselau <mooseliu@hotmail.co.uk>
2018-04-24 00:23:32 +0800

    Update home page
    Complete on like event function.

--- miniPro/mApp/pages/home/home.js
+++ miniPro/mApp/pages/home/home.js
@@ -9,38 +9,63 @@ Page({
    * 页面的初始数据
    */
   data: {
-    banner:[],
-    hotSongs:[],
-    title:"hello"
+    banner: [],
+    hotSongs: [],
+    title: "hello"
   },
 
   getIndexData: function () {
     let that = this;
-    util.request(api.IndexUrl,{}, 'POST').then(function (res) {
+    util.request(api.IndexUrl, {}, 'POST').then(function (res) {
       console.log("getIndexData", res);
       that.setData({
-          banner: res.banner,
-          hotSongs: res.hotSongs,
-          title: res.title
-        });
-      wx.hideLoading()    
-    }).catch(err=>{
-      wx.hideLoading() 
+        banner: res.banner,
+        hotSongs: res.hotSongs,
+        title: res.title
+      });
+      wx.hideLoading()
+    }).catch(err => {
+      wx.hideLoading()
       util.showErrorToast("加载失败...")
       console.log("err", err);
     });
   },
 
-/**
- * Hot song like event
- */
+  /**
+   * Hot song like event: switch image & text
+   */
   onLikeSong: function (event) {
+    var that = this;
     console.log("called from --> hot song like event");
-    var itemid = event.target.id
-    var query = wx.createSelectorQuery();
-    console.log(query.select("'#" + itemid + "'"));
-    // 提前准备好 每个hot song 对应的 image url属性
-    // 点击like，使用setData来换image
+    var imgIndex = event.currentTarget.dataset.imgIndex;
+    var isCollected = event.currentTarget.dataset.isCollected;
+    this.switchImgAndText(imgIndex, isCollected);
+  },
+
+  /* auxiliary method for onLikeSong */
+  switchImgAndText: function (imgIndex, isCollected) {
+    var that = this;
+    var likeImg = "../../images/icons/icon_collect_checked.png";
+    var notlikeImg = "../../images/icons/icon_collect.png";
+
+    /* the dynamic way to set data is to use an array */
+    var targetImgSrc = "array[" + imgIndex + "]" + ".imgSrc";
+    var targetLikeText = "array[" + imgIndex + "]" + ".likeText";
+    var targetCollectedFlag = "array[" + imgIndex + "]" + ".isCollected";
+    // console.log(isCollected);
+    if (isCollected) {
+      that.setData({
+        [targetImgSrc]: notlikeImg,
+        [targetCollectedFlag]: false,
+        [targetLikeText]: "收藏"
+      });
+    } else {
+      that.setData({
+        [targetImgSrc]: likeImg,
+        [targetCollectedFlag]: true,
+        [targetLikeText]: "已收藏"
+      });
+    }
   },
 
   /**
@@ -50,13 +75,14 @@ Page({
     var that = this;
     let fromLocal = app.globalData.useLocal;
     console.log("localData", localData);
-    if(fromLocal){
-        that.setData({
-          banner:localData.banner,
-          hotSongs:localData.hotSongs
-        });
+    if (fromLocal) {
+      that.setData({
+        array: localData.array,
+        banner: localData.banner,
+        hotSongs: localData.hotSongs,
+      });
     }
-    else{
+    else {
       wx.showLoading({
         title: '正在加载.....',
       })
@@ -68,48 +94,48 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   }
 })
