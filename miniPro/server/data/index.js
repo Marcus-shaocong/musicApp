6e9ee7e0463c68175d06f5b5203a3c00479b37b2
marcus-shaocong <marcus-shaocong@LIANGS4.local>
2018-05-07 02:36:42 +0800

    add more songs

--- miniPro/server/data/index.js
+++ miniPro/server/data/index.js
@@ -13,12 +13,18 @@ module.exports={
           singer: "陈奕迅",
           categoryId: 10010,
           gallery: [
-            { time: 10.502, image: `${Appconfig.serverDomain}/assets/images/wenrou_1.png` },
-            { time: 11.101, image: `${Appconfig.serverDomain}/assets/images/wenrou_2.png` },
-            { time: 16.262, image: `${Appconfig.serverDomain}/assets/images/wenrou_3.png` },
-            { time: 19.563, image: `${Appconfig.serverDomain}/assets/images/wenrou_4.png` },
-            { time: 22.968, image: `${Appconfig.serverDomain}/assets/images/wenrou_5.png` },
-            { time: 22.968, image: `${Appconfig.serverDomain}/assets/images/wenrou_6.png` },
+            { time: 10.502, image: `${Appconfig.serverDomain}/assets/images/wenrou_v3_1.png` },
+            { time: 11.101, image: `${Appconfig.serverDomain}/assets/images/wenrou_v3_2.png` },
+            { time: 16.262, image: `${Appconfig.serverDomain}/assets/images/wenrou_v3_3.png` },
+            { time: 19.563, image: `${Appconfig.serverDomain}/assets/images/wenrou_v3_4.png` },
+            { time: 22.968, image: `${Appconfig.serverDomain}/assets/images/wenrou_v3_5.png` },
+            { time: 22.968, image: `${Appconfig.serverDomain}/assets/images/wenrou_v3_6.png` },
+            { time: 10.502, image: `${Appconfig.serverDomain}/assets/images/wenrou_v3_7.png` },
+            { time: 11.101, image: `${Appconfig.serverDomain}/assets/images/wenrou_v3_8.png` },
+            { time: 16.262, image: `${Appconfig.serverDomain}/assets/images/wenrou_v3_9.png` },
+            { time: 19.563, image: `${Appconfig.serverDomain}/assets/images/wenrou_v3_10.png` },
+            { time: 22.968, image: `${Appconfig.serverDomain}/assets/images/wenrou_v3_11.png` },
+            { time: 22.968, image: `${Appconfig.serverDomain}/assets/images/wenrou_v3_12.png` }
           ], // 系列相关图片
           listPicUrl:`${Appconfig.serverDomain}/assets/images/10011.jpg`,
           src:`${Appconfig.serverDomain}/assets/mp3/wenyou.mp3`,
@@ -30,12 +36,18 @@ module.exports={
           singer: "邓紫棋",
           categoryId: 10020,
           gallery: [
-            { time: 10.502, image: `${Appconfig.serverDomain}/assets/images/wenrou_1.png` },
-            { time: 11.101, image: `${Appconfig.serverDomain}/assets/images/wenrou_2.png` },
-            { time: 16.262, image: `${Appconfig.serverDomain}/assets/images/wenrou_3.png` },
-            { time: 19.563, image: `${Appconfig.serverDomain}/assets/images/wenrou_4.png` },
-            { time: 22.968, image: `${Appconfig.serverDomain}/assets/images/wenrou_5.png` },
-            { time: 22.968, image: `${Appconfig.serverDomain}/assets/images/wenrou_6.png` },
+            { time: 10.502, image: `${Appconfig.serverDomain}/assets/images/wenrou_v3_1.png` },
+            { time: 11.101, image: `${Appconfig.serverDomain}/assets/images/wenrou_v3_2.png` },
+            { time: 16.262, image: `${Appconfig.serverDomain}/assets/images/wenrou_v3_3.png` },
+            { time: 19.563, image: `${Appconfig.serverDomain}/assets/images/wenrou_v3_4.png` },
+            { time: 22.968, image: `${Appconfig.serverDomain}/assets/images/wenrou_v3_5.png` },
+            { time: 22.968, image: `${Appconfig.serverDomain}/assets/images/wenrou_v3_6.png` },
+            { time: 10.502, image: `${Appconfig.serverDomain}/assets/images/wenrou_v3_7.png` },
+            { time: 11.101, image: `${Appconfig.serverDomain}/assets/images/wenrou_v3_8.png` },
+            { time: 16.262, image: `${Appconfig.serverDomain}/assets/images/wenrou_v3_9.png` },
+            { time: 19.563, image: `${Appconfig.serverDomain}/assets/images/wenrou_v3_10.png` },
+            { time: 22.968, image: `${Appconfig.serverDomain}/assets/images/wenrou_v3_11.png` },
+            { time: 22.968, image: `${Appconfig.serverDomain}/assets/images/wenrou_v3_12.png` }
           ], // 系列相关图片
           listPicUrl: `${Appconfig.serverDomain}/assets/images/10021.jpg`,
           src:`${Appconfig.serverDomain}/assets/mp3/wenyou.mp3`,
@@ -47,12 +59,18 @@ module.exports={
           singer: "王力宏",
           categoryId: 10030,
           gallery: [
-            { time: 10.502, image: `${Appconfig.serverDomain}/assets/images/wenrou_1.png` },
-            { time: 11.101, image: `${Appconfig.serverDomain}/assets/images/wenrou_2.png` },
-            { time: 16.262, image: `${Appconfig.serverDomain}/assets/images/wenrou_3.png` },
-            { time: 19.563, image: `${Appconfig.serverDomain}/assets/images/wenrou_4.png` },
-            { time: 22.968, image: `${Appconfig.serverDomain}/assets/images/wenrou_5.png` },
-            { time: 22.968, image: `${Appconfig.serverDomain}/assets/images/wenrou_6.png` },
+            { time: 10.502, image: `${Appconfig.serverDomain}/assets/images/wenrou_v3_1.png` },
+            { time: 11.101, image: `${Appconfig.serverDomain}/assets/images/wenrou_v3_2.png` },
+            { time: 16.262, image: `${Appconfig.serverDomain}/assets/images/wenrou_v3_3.png` },
+            { time: 19.563, image: `${Appconfig.serverDomain}/assets/images/wenrou_v3_4.png` },
+            { time: 22.968, image: `${Appconfig.serverDomain}/assets/images/wenrou_v3_5.png` },
+            { time: 22.968, image: `${Appconfig.serverDomain}/assets/images/wenrou_v3_6.png` },
+            { time: 10.502, image: `${Appconfig.serverDomain}/assets/images/wenrou_v3_7.png` },
+            { time: 11.101, image: `${Appconfig.serverDomain}/assets/images/wenrou_v3_8.png` },
+            { time: 16.262, image: `${Appconfig.serverDomain}/assets/images/wenrou_v3_9.png` },
+            { time: 19.563, image: `${Appconfig.serverDomain}/assets/images/wenrou_v3_10.png` },
+            { time: 22.968, image: `${Appconfig.serverDomain}/assets/images/wenrou_v3_11.png` },
+            { time: 22.968, image: `${Appconfig.serverDomain}/assets/images/wenrou_v3_12.png` }
           ], // 系列相关图片
           listPicUrl: `${Appconfig.serverDomain}/assets/images/10031.jpg`,
           src:`${Appconfig.serverDomain}/assets/mp3/wenyou.mp3`,
@@ -64,6 +82,29 @@ module.exports={
           singer: "五月天",
           categoryId: 10040,
           gallery: [
+            { time: 10.502, image: `${Appconfig.serverDomain}/assets/images/wenrou_v3_1.png` },
+            { time: 11.101, image: `${Appconfig.serverDomain}/assets/images/wenrou_v3_2.png` },
+            { time: 16.262, image: `${Appconfig.serverDomain}/assets/images/wenrou_v3_3.png` },
+            { time: 19.563, image: `${Appconfig.serverDomain}/assets/images/wenrou_v3_4.png` },
+            { time: 22.968, image: `${Appconfig.serverDomain}/assets/images/wenrou_v3_5.png` },
+            { time: 22.968, image: `${Appconfig.serverDomain}/assets/images/wenrou_v3_6.png` },
+            { time: 10.502, image: `${Appconfig.serverDomain}/assets/images/wenrou_v3_7.png` },
+            { time: 11.101, image: `${Appconfig.serverDomain}/assets/images/wenrou_v3_8.png` },
+            { time: 16.262, image: `${Appconfig.serverDomain}/assets/images/wenrou_v3_9.png` },
+            { time: 19.563, image: `${Appconfig.serverDomain}/assets/images/wenrou_v3_10.png` },
+            { time: 22.968, image: `${Appconfig.serverDomain}/assets/images/wenrou_v3_11.png` },
+            { time: 22.968, image: `${Appconfig.serverDomain}/assets/images/wenrou_v3_12.png` }
+          ], // 系列相关图片
+          listPicUrl: `${Appconfig.serverDomain}/assets/images/10041.jpeg`,
+          src:`${Appconfig.serverDomain}/assets/mp3/wenyou.mp3`,
+          hardLevel:2
+        },
+        {
+          id: 10051,
+          name: "over and over",
+          singer: "Rachael Yamagata",
+          categoryId: 10040,
+          gallery: [
             { time: 10.502, image: `${Appconfig.serverDomain}/assets/images/wenrou_1.png` },
             { time: 11.101, image: `${Appconfig.serverDomain}/assets/images/wenrou_2.png` },
             { time: 16.262, image: `${Appconfig.serverDomain}/assets/images/wenrou_3.png` },
@@ -71,10 +112,164 @@ module.exports={
             { time: 22.968, image: `${Appconfig.serverDomain}/assets/images/wenrou_5.png` },
             { time: 22.968, image: `${Appconfig.serverDomain}/assets/images/wenrou_6.png` },
           ], // 系列相关图片
-          listPicUrl: `${Appconfig.serverDomain}/assets/images/10041.jpeg`,
-          src:`${Appconfig.serverDomain}/assets/mp3/wenyou.mp3`,
+          listPicUrl: `${Appconfig.serverDomain}/assets/images/over_and_over.jpg`,
+          src:`${Appconfig.serverDomain}/assets/mp3/over_and_over.mp3`,
           hardLevel:2
+        },
+        {
+          id: 10052,
+          name: "Carry on",
+          singer: "Janelle Monáe",
+          categoryId: 10040,
+          gallery: [
+            { time: 10.502, image: `${Appconfig.serverDomain}/assets/images/wenrou_1.png` },
+            { time: 11.101, image: `${Appconfig.serverDomain}/assets/images/wenrou_2.png` },
+            { time: 16.262, image: `${Appconfig.serverDomain}/assets/images/wenrou_3.png` },
+            { time: 19.563, image: `${Appconfig.serverDomain}/assets/images/wenrou_4.png` },
+            { time: 22.968, image: `${Appconfig.serverDomain}/assets/images/wenrou_5.png` },
+            { time: 22.968, image: `${Appconfig.serverDomain}/assets/images/wenrou_6.png` },
+          ], // 系列相关图片
+          listPicUrl: `${Appconfig.serverDomain}/assets/images/carry_on.jpg`,
+          src:`${Appconfig.serverDomain}/assets/mp3/carry_on.mp3`,
+          hardLevel:2
+        },
+        {
+          id: 10061,
+          name: "I wanna hold your hand",
+          singer: "The Beatles Tribute",
+          categoryId: 10040,
+          gallery: [
+            { time: 10.502, image: `${Appconfig.serverDomain}/assets/images/wenrou_1.png` },
+            { time: 11.101, image: `${Appconfig.serverDomain}/assets/images/wenrou_2.png` },
+            { time: 16.262, image: `${Appconfig.serverDomain}/assets/images/wenrou_3.png` },
+            { time: 19.563, image: `${Appconfig.serverDomain}/assets/images/wenrou_4.png` },
+            { time: 22.968, image: `${Appconfig.serverDomain}/assets/images/wenrou_5.png` },
+            { time: 22.968, image: `${Appconfig.serverDomain}/assets/images/wenrou_6.png` },
+          ], // 系列相关图片
+          listPicUrl: `${Appconfig.serverDomain}/assets/images/I_wanna_hold_your_hand.jpg`,
+          src:`${Appconfig.serverDomain}/assets/mp3/I_wanna_hold_your_hand.mp3`,
+          hardLevel:2
+        },
+        {
+          id: 10071,
+          name: "Call me maybe",
+          singer: "Tiffany Alvord",
+          categoryId: 10040,
+          gallery: [
+            { time: 10.502, image: `${Appconfig.serverDomain}/assets/images/wenrou_1.png` },
+            { time: 11.101, image: `${Appconfig.serverDomain}/assets/images/wenrou_2.png` },
+            { time: 16.262, image: `${Appconfig.serverDomain}/assets/images/wenrou_3.png` },
+            { time: 19.563, image: `${Appconfig.serverDomain}/assets/images/wenrou_4.png` },
+            { time: 22.968, image: `${Appconfig.serverDomain}/assets/images/wenrou_5.png` },
+            { time: 22.968, image: `${Appconfig.serverDomain}/assets/images/wenrou_6.png` },
+          ], // 系列相关图片
+          listPicUrl: `${Appconfig.serverDomain}/assets/images/call_me_maybe.jpg`,
+          src:`${Appconfig.serverDomain}/assets/mp3/call_me_maybe.mp3`,
+          hardLevel:2
+        } ,
+        {
+          id: 10081,
+          name: "It is heartache",
+          singer: "Rod Stewart",
+          categoryId: 10040,
+          gallery: [
+            { time: 10.502, image: `${Appconfig.serverDomain}/assets/images/wenrou_1.png` },
+            { time: 11.101, image: `${Appconfig.serverDomain}/assets/images/wenrou_2.png` },
+            { time: 16.262, image: `${Appconfig.serverDomain}/assets/images/wenrou_3.png` },
+            { time: 19.563, image: `${Appconfig.serverDomain}/assets/images/wenrou_4.png` },
+            { time: 22.968, image: `${Appconfig.serverDomain}/assets/images/wenrou_5.png` },
+            { time: 22.968, image: `${Appconfig.serverDomain}/assets/images/wenrou_6.png` },
+          ], // 系列相关图片
+          listPicUrl: `${Appconfig.serverDomain}/assets/images/it_is_heartache.jpg`,
+          src:`${Appconfig.serverDomain}/assets/mp3/It_is_hearache.mp3`,
+          hardLevel:2
+        },
+        {
+          id: 10091,
+          name: "Marry you",
+          singer: "Bruno Mars",
+          categoryId: 10040,
+          gallery: [
+            { time: 10.502, image: `${Appconfig.serverDomain}/assets/images/wenrou_1.png` },
+            { time: 11.101, image: `${Appconfig.serverDomain}/assets/images/wenrou_2.png` },
+            { time: 16.262, image: `${Appconfig.serverDomain}/assets/images/wenrou_3.png` },
+            { time: 19.563, image: `${Appconfig.serverDomain}/assets/images/wenrou_4.png` },
+            { time: 22.968, image: `${Appconfig.serverDomain}/assets/images/wenrou_5.png` },
+            { time: 22.968, image: `${Appconfig.serverDomain}/assets/images/wenrou_6.png` },
+          ], // 系列相关图片
+          listPicUrl: `${Appconfig.serverDomain}/assets/images/marry_you.jpg`,
+          src:`${Appconfig.serverDomain}/assets/mp3/marry_you.mp3`,
+          hardLevel:2
+        },
+        {
+          id: 10092,
+          name: "Let it be",
+          singer: "Paul McCartney",
+          categoryId: 10040,
+          gallery: [
+            { time: 10.502, image: `${Appconfig.serverDomain}/assets/images/wenrou_1.png` },
+            { time: 11.101, image: `${Appconfig.serverDomain}/assets/images/wenrou_2.png` },
+            { time: 16.262, image: `${Appconfig.serverDomain}/assets/images/wenrou_3.png` },
+            { time: 19.563, image: `${Appconfig.serverDomain}/assets/images/wenrou_4.png` },
+            { time: 22.968, image: `${Appconfig.serverDomain}/assets/images/wenrou_5.png` },
+            { time: 22.968, image: `${Appconfig.serverDomain}/assets/images/wenrou_6.png` },
+          ], // 系列相关图片
+          listPicUrl: `${Appconfig.serverDomain}/assets/images/let_it_be.jpg`,
+          src:`${Appconfig.serverDomain}/assets/mp3/let_it_be.mp3`,
+          hardLevel:2
+        },
+        {
+          id: 10093,
+          name: "No woman no cry",
+          singer: "Bob Marley",
+          categoryId: 10040,
+          gallery: [
+            { time: 10.502, image: `${Appconfig.serverDomain}/assets/images/wenrou_1.png` },
+            { time: 11.101, image: `${Appconfig.serverDomain}/assets/images/wenrou_2.png` },
+            { time: 16.262, image: `${Appconfig.serverDomain}/assets/images/wenrou_3.png` },
+            { time: 19.563, image: `${Appconfig.serverDomain}/assets/images/wenrou_4.png` },
+            { time: 22.968, image: `${Appconfig.serverDomain}/assets/images/wenrou_5.png` },
+            { time: 22.968, image: `${Appconfig.serverDomain}/assets/images/wenrou_6.png` },
+          ], // 系列相关图片
+          listPicUrl: `${Appconfig.serverDomain}/assets/images/no_woman_no_cry.jpg`,
+          src:`${Appconfig.serverDomain}/assets/mp3/no_woman_no_cry.mp3`,
+          hardLevel:2
+        },
+        {
+          id: 10094,
+          name: "We are young",
+          singer: "Janelle Monáe",
+          categoryId: 10040,
+          gallery: [
+            { time: 10.502, image: `${Appconfig.serverDomain}/assets/images/wenrou_1.png` },
+            { time: 11.101, image: `${Appconfig.serverDomain}/assets/images/wenrou_2.png` },
+            { time: 16.262, image: `${Appconfig.serverDomain}/assets/images/wenrou_3.png` },
+            { time: 19.563, image: `${Appconfig.serverDomain}/assets/images/wenrou_4.png` },
+            { time: 22.968, image: `${Appconfig.serverDomain}/assets/images/wenrou_5.png` },
+            { time: 22.968, image: `${Appconfig.serverDomain}/assets/images/wenrou_6.png` },
+          ], // 系列相关图片
+          listPicUrl: `${Appconfig.serverDomain}/assets/images/we_are_young.jpg`,
+          src:`${Appconfig.serverDomain}/assets/mp3/we_are_young.mp3`,
+          hardLevel:4
+        },
+        {
+          id: 10095,
+          name: "I believe I can fly",
+          singer: "Ronan Keating",
+          categoryId: 10040,
+          gallery: [
+            { time: 10.502, image: `${Appconfig.serverDomain}/assets/images/wenrou_1.png` },
+            { time: 11.101, image: `${Appconfig.serverDomain}/assets/images/wenrou_2.png` },
+            { time: 16.262, image: `${Appconfig.serverDomain}/assets/images/wenrou_3.png` },
+            { time: 19.563, image: `${Appconfig.serverDomain}/assets/images/wenrou_4.png` },
+            { time: 22.968, image: `${Appconfig.serverDomain}/assets/images/wenrou_5.png` },
+            { time: 22.968, image: `${Appconfig.serverDomain}/assets/images/wenrou_6.png` },
+          ], // 系列相关图片
+          listPicUrl: `${Appconfig.serverDomain}/assets/images/I_believe_I_can_fly.jpg`,
+          src:`${Appconfig.serverDomain}/assets/mp3/I_belive_I_can_fly.mp3`,
+          hardLevel:3
         } 
+
       ],
       title: "MusicApp"
   }
