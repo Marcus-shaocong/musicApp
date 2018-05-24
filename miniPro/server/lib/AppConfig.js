e0e8169182ae2afd7d52a34607b2309d74a4ab22
Liang <Shaocong.Liang@emc.com>
2018-05-25 03:12:33 +0800

    fix the https problem

--- miniPro/server/lib/AppConfig.js
+++ miniPro/server/lib/AppConfig.js
@@ -1,11 +1,11 @@
 let AppConfig = {
     production: {
-        serverDomain:"http://xinjushi.xyz",
+        serverDomain:"https://xinjushi.xyz",
         appId: "wxecb99e4db814c055",
         appSecret: "c1553926d21e7caf9391b2c9e314c1a1"
     },
     dev: {
-        serverDomain:"http://localhost:3000",
+        serverDomain:"https://localhost:3000",
         appId: "wxecb99e4db814c055",
         appSecret: "c1553926d21e7caf9391b2c9e314c1a1" 
     }
